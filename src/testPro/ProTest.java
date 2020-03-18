a02973ed20c2a6262e46fa5989f880236a2a37e4
VitalZajka <vitali.zaika1@gmail.com>
2020-03-17 21:13:16 -0400

    if statement is added co class ProTest

--- src/testPro/ProTest.java
+++ src/testPro/ProTest.java
@@ -3,5 +3,10 @@ package testPro;
 public class ProTest {
 public static void main(String[] args) {
 	
+	boolean isRaining=false;
+	
+	if(!isRaining) {
+		System.out.println("It is clear");
+	}
 }
 }
